d76fa80cba62dc66a29dd9d50ad17cdfebd39aee
izapolsk <izapolsk@redhat.com>
2019-01-09 18:22:06 +0200

    final commit with nginx changes after testing

--- xstartup.sh
+++ xstartup.sh
@@ -1,9 +1,12 @@
 #!/bin/sh
 
 export DISPLAY=:99
-export PATH="/usr/bin:/root/firefox:/root/chrome-driver:$PATH"
+export PATH="/usr/bin:/root/firefox:/root/chrome-driver:/opt/google/chrome:$PATH"
 
 vncserver ${DISPLAY} -Log *:stderr:100 >> /allout.txt
 
-# Start the selenium server
-java -jar /root/selenium-server/selenium-server-standalone.jar 2>&1 | tee -a /allout.txt
+# start nginx which will proxy requests to selenium server
+nginx
+
+# start the selenium server
+java -jar /root/selenium-server/selenium-server-standalone.jar -port 4445 2>&1 | tee -a /allout.txt
